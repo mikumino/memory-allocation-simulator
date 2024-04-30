8995036b5112d62ec1d438368c6e88247c2e7846
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-04-30 01:05:14 -0700

    Update Benchmark.jsx

--- ui/src/routes/Benchmark.jsx
+++ ui/src/routes/Benchmark.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function Benchmark() {
+    const [numTrials, setNumTrials] = useState(10);
+
     return (
         <div>
-            <h1>Benchmark</h1>
+            <div className="flex flex-col max-w-6xl p-4 mx-auto">
+                <h1 className="text-2xl font-bold">Benchmark</h1>
+            </div>
         </div>
     );
 }
