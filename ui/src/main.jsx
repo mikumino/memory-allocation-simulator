5fca1165d9e86c232a71d79ef621328026fdc048
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-04-30 00:29:45 -0700

    benchmark

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Benchmark from './routes/Benchmark.jsx'
+import Navbar from './components/Navbar.jsx'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/benchmark",
+        element: <Benchmark />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ ui/src/routes/Benchmark.jsx
@@ -0,0 +1,9 @@
+function Benchmark() {
+    return (
+        <div>
+            <h1>Benchmark</h1>
+        </div>
+    );
+}
+
+export default Benchmark;
